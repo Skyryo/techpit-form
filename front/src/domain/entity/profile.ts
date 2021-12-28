34eeb6915bdce6703ac16e6099ee2d26446a298a
ryo sakai <92973002+Skyryo@users.noreply.github.com>
2021-12-28 17:34:29 +0900

    1228_2-2_finished

---
+++ front/src/domain/entity/profile.ts
@@ -0,0 +1,10 @@
+import { Address } from "./address";
+import { Gender } from "./gender";
+
+export type Profile = {
+    name: string;
+    description: string;
+    birthday: string;
+    gender: Gender;
+    address: Address;
+};
---
+++ front/src/store/profile/reducer.ts
@@ -0,0 +1,29 @@
+import { reducerWithInitialState } from "typescript-fsa-reducers";
+import { Profile } from "../../domain/entity/profile";
+import profileActions from "./action";
+
+const init: Profile = {
+    name: "",
+    description:"",
+    birthday:"",
+    gender:"",
+
+    address: {
+        postalcode: "",
+        prefecture: "",
+        city: "",
+        restAddress: ""
+    }
+};
+
+const profileReducer = reducerWithInitialState(init)
+    .case(profileActions.setProfile,(state, payload) => ({
+            ...state,
+            ...payload
+        }))
+    .case(profileActions.setAddress,(state, payload) => ({
+            ...state,
+            address: {...state.address, ...payload }
+        }));
+
+export default profileReducer;
